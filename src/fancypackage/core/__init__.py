517d07f6ff118a41c3fff9254ca83a318c76f1f6
Jimènez Correa <sarajimenez>
2024-11-07 17:38:46 +0100

    update

--- src/fancypackage/core/__init__.py
+++
@@ -1,3 +0,0 @@
-from ._constants import DATA_DIR, FIG_DIR
-
-__all__ = ["DATA_DIR", "FIG_DIR"]
--- src/fancypackage/core/_constants.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-ROOT = Path(__file__).parents[3].resolve()
-
-
-DATA_DIR = ROOT / "data"
-FIG_DIR = ROOT / "figures"
